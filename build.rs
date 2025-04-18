4183dd1de9cb379aa3ef2b38923021a07b51035d
suziwei <su.ziwei@outlook.com>
2025-04-18 17:23:05 -0500

    Updated build.rs

--- build.rs
+++ build.rs
@@ -4,9 +4,17 @@ use std::fs::read_dir;
 use std::path::Path;
 
 fn main() {
+    if env::var("TARGET").unwrap().contains("apple-darwin") {
+        println!("cargo:warning=Building for macOS");
+        env::set_var("CC", "gcc");
+        println!("cargo:rustc-link-search=native=/usr/local/lib");
+        println!("cargo:rustc-link-lib=dylib=omp");
+    }
+    else {
+        println!("cargo:rustc-link-search=native=/usr/lib");
+        println!("cargo:rustc-link-lib=dylib=gomp");
+    }
     println!("cargo:include=/usr/include");
-    println!("cargo:rustc-link-search=native=/usr/lib");
-    println!("cargo:rustc-link-lib=dylib=gomp");
     println!("cargo:rustc-link-lib=dylib=blas");
 
     let ss_dir = clone_suitesparse();
